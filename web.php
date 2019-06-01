ebf7279a75fb98c5ea36f9f435c980660dabfdae
SaurabhNandy <39880387+SaurabhNandy@users.noreply.github.com>
2019-06-01 19:10:35 +0530

    Update web.php

--- web.php
+++ web.php
@@ -1,28 +1,7 @@
 <?php	
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$conn = new mysqli($servername, $username, $password);
+	$conn = new mysqli("localhost","root","","PlaystationHub");
 	if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
-	}
-
-	$sql = "CREATE DATABASE IF NOT EXISTS myDB";
-	if ($conn->query($sql) === TRUE) {
-	} else {
-	echo "Error creating database: " . $conn->error;
-	}
-	
-	$dbname = "myDB";
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	} 
-
-	$sql = "CREATE TABLE IF NOT EXISTS myT(name VARCHAR(30) NOT NULL,email VARCHAR(30) NOT NULL,message VARCHAR(30) NOT NULL)";
-	if ($conn->query($sql) === TRUE) {
-	} else {
-	echo "Error creating table: " . $conn->error;
 	}
 
 	if($_POST['loginbtn'])
@@ -30,13 +9,14 @@
   		$name = $_POST['fname'];
   		$email = $_POST['email'];
   		$message = $_POST['text'];
-  		$sql = "INSERT INTO myT (name,email,message)VALUES ('$name','$email','$message')";
+  		$sql = "INSERT INTO  Fedback(name,email,Message)VALUES ('$name','$email','$message')";
 
-		if ($conn->query($sql) === TRUE) {
+		if ($conn->query($sql)) {
 		//echo "New record created successfully <br> $name - $email - $message";
 		} else {
 		echo "Error: " . $sql . "<br>" . $conn->error;
 		}	
   	}
-	$conn->close();
-	?>
+  	header("location:index.html");
+  	$conn->close();
+?>
